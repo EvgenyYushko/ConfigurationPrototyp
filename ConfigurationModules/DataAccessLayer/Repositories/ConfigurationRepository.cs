507b94c6d1d17195ea67714c59d6de5540aa07c1
Yushko_YM <Yushko_YM@ST.BY>
2022-05-27 11:02:40 +0300

    Вынес конфиги во внешнюю папку

--- ConfigurationModules/DataAccessLayer/Repositories/ConfigurationRepository.cs
+++ ConfigurationModules/DataAccessLayer/Repositories/ConfigurationRepository.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using ConfigurationModules.DomainLayer.Models.Application;
 using ConfigurationModules.DomainLayer.Models.Base;
@@ -10,52 +12,72 @@ namespace ConfigurationModules.DataAccessLayer.Repositories
 {
     public class ConfigurationRepository : IConfigurationRepository
     {
-        private Configuration _context = null;
+        private const string APP_DATA = "AppData";
+        private const string CONFIG_FOLDER_NAME = "PrototypeConfigs";
+        private const string CONFIG_FILE_NAME = "App.config";
+
+        private readonly string _configFilePath;
+        private Configuration _config;
 
         public ConfigurationRepository()
         {
-            SetContext();
+            _configFilePath = Path.Combine(Environment.GetEnvironmentVariable(APP_DATA), CONFIG_FOLDER_NAME, CONFIG_FILE_NAME);
         }
 
-        private void SetContext()
+        private Configuration Configuration => _config ??= InitConfig();
+
+        private Configuration InitConfig()
         {
-            _context = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var fileMap = new ExeConfigurationFileMap
+            {
+                ExeConfigFilename = _configFilePath
+            };
+
+            return ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None); 
         }
 
         public ApplicationConfigSettings GetDefaultApplicationSettings()
         {
-            var configSettings = (ApplicationSettings)ConfigurationManager.GetSection(nameof(ApplicationSettings));
+            var configSettings = (ApplicationSettings)GetSection(nameof(ApplicationSettings));
             return configSettings.Settings;
         }
 
         public ApplicationConfigSettings GetProfile(string profileName)
         {
-            var profilesSettings = (ProfilesSettings)_context.Sections[nameof(ProfilesSettings)];
-            return (ApplicationConfigSettings)profilesSettings.Profiles.Get(profileName);
+            var profilesSettings = GetProfilesSettings();
+            return (ApplicationConfigSettings)profilesSettings.Get(profileName);
         }
 
         public List<ApplicationConfigSettings> GetProfiles()
         {
-            var profileSettings = (ProfilesSettings)ConfigurationManager.GetSection(nameof(ProfilesSettings));
-            return profileSettings.Profiles.OfType<ApplicationConfigSettings>().ToList();
+            var profilesSettings = GetProfilesSettings();
+            return profilesSettings.OfType<ApplicationConfigSettings>().ToList();
         }
 
         public void AddProfile(ApplicationConfigSettings newProfile)
         {
-            var profilesSettings = (ProfilesSettings)_context.Sections[nameof(ProfilesSettings)];
-            profilesSettings.Profiles.Add(newProfile);
+            var profilesSettings = GetProfilesSettings();
+            profilesSettings.Add(newProfile);
         }
 
         public void DeleteProfile(string newProfile)
         {
-            SetContext();
-            var profilesSettings = (ProfilesSettings)_context.Sections[nameof(ProfilesSettings)];
-            profilesSettings.Profiles.Delete(newProfile);
+            var profilesSettings = GetProfilesSettings();
+            profilesSettings.Delete(newProfile);
         }
+
+        private Profiles GetProfilesSettings()
+        {
+            var profilesSection = (ProfilesSettings)GetSection(nameof(ProfilesSettings));
+            return profilesSection.Profiles;
+        }
+
+        private ConfigurationSection GetSection(string sectionName) => Configuration.Sections[sectionName];
 
         public void Save()
         {
-            _context.Save(ConfigurationSaveMode.Modified);
+            Configuration.Save(ConfigurationSaveMode.Modified);
+            _config = null;
         }
 
         #region Test
--- ConfigurationPrototyp/AppCustomForm.cs
+++ ConfigurationPrototyp/AppCustomForm.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using AutoMapper;
 using ConfigurationModules.ServiceLayer.Models;
 using ConfigurationModules.ServiceLayer.Services;
 
